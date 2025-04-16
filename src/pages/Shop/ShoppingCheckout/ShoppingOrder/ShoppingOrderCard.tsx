d8fb10ae05cb055cd47e8a9f0a1ea79b899365d9
Rahmonaliyev Abdulaziz <142968268+RahmonaliyevAbdulaziz@users.noreply.github.com>
2025-04-16 14:04:22 +0500

    checkout card fixed

--- src/pages/Shop/ShoppingCheckout/ShoppingOrder/ShoppingOrderCard.tsx
+++ src/pages/Shop/ShoppingCheckout/ShoppingOrder/ShoppingOrderCard.tsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
 
 export default function ShoppingOrderCard({ product }: any) {
+    const navigate = useNavigate();
+    const category = product.title === 'Peace Lil' ? 'house-plants' : product.category;
+    
     return (
-        <li key={product._id} className='flex justify-between items-center my-2 bg-[#FBFBFB] px-2 py-1 rounded'>
+        <li onClick={()=> navigate(`/aboutProduct/${category}/${product._id}`)} key={product._id} className='flex cursor-pointer justify-between items-center my-2 bg-[#FBFBFB] px-2 py-1 rounded'>
             <div className=' flex items-center gap-3'>
                 <div className='w-12 rounded h-12 object-contain overflow-hidden flex justify-center items-center'><img src={product.main_image} alt="greenshop image flower img" className='w-full h-auto object-contain mix-blend-multiply scale-100 group-hover:scale-110 transi' /></div>
                 <div className='flex flex-col'>
@@ -18,4 +22,3 @@ export default function ShoppingOrderCard({ product }: any) {
         </li>
     )
 }
-
